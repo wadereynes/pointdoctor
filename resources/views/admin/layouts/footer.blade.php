39725ac75f4afc088737f850916da5cbddf96780
Wade Reynes <wadeshow25@gmail.com>
2025-02-25 21:03:42 +0800

    15.75

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -124,6 +124,7 @@
                     format: 'YYYY-MM-DD'
                 })
             })
+        </script>
         <script>
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
